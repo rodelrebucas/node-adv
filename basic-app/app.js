f17144800145e30066345547c99834bc3069c514
rodel <rodelrebucas@gmail.com>
2019-02-05 13:54:20 +0800

    add routes and npm scripts

--- basic-app/app.js
+++ basic-app/app.js
@@ -1,36 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html"); // set a response type
-  const url = req.url;
-  if (url === "/") {
-    // create a response
-    res.write(
-      "<html><body><h1>Hello Node!</h1><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>send</button></form></body></html>"
-    );
-    return res.end();
-  }
-
-  if (url === "/message" && req.method === "POST") {
-    // parse the post request
-    // process data until complete (node's job)
-    // listen to data events
-    const body = [];
-    req.on("data", chunk => {
-      console.log("chunk:", chunk); // buffer, temporary memory location
-      body.push(chunk);
-    });
-    // gather the data
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      fs.writeFileSync("message.txt", parsedBody.split("=")[1]);
-    });
-
-    res.statusCode = 302; // redirect
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(5000);
