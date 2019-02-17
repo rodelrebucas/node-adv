e4e07404f0566ec8367c8359e7cfdd984aa3127f
rodel <rodelrebucas@gmail.com>
2019-02-18 03:08:10 +0800

    route protection

--- express-shop-app/routes/admin.js
+++ express-shop-app/routes/admin.js
@@ -5,12 +5,13 @@ const router = express.Router();
 
 // const rootDir = require("../util/path");
 const adminController = require("../controllers/admin");
+const isAuth = require("../middleware/is-auth");
 
-router.get("/add-product", adminController.getAddProduct);
-router.get("/products", adminController.getProducts);
-router.post("/add-product", adminController.postAddProduct);
-router.get("/edit-product/:id", adminController.getEditProduct);
-router.post("/edit-product", adminController.postEditProduct);
-router.post("/delete-product", adminController.postDeleteProduct);
+router.get("/add-product", isAuth, adminController.getAddProduct);
+router.get("/products", isAuth, adminController.getProducts);
+router.post("/add-product", isAuth, adminController.postAddProduct);
+router.get("/edit-product/:id", isAuth, adminController.getEditProduct);
+router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 // module.exports = router;
 module.exports = router;
--- express-shop-app/routes/shop.js
+++ express-shop-app/routes/shop.js
@@ -4,14 +4,15 @@ const path = require("path");
 const router = express.Router();
 
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/product/:id", shopController.getProduct);
-router.get("/cart", shopController.getCart);
-router.post("/cart", shopController.postCart);
-router.post("/cart-delete-item", shopController.postDeleteCartItem);
-router.get("/orders", shopController.getOrders);
-router.post("/create-order", shopController.postOrder);
+router.get("/cart", isAuth, shopController.getCart);
+router.post("/cart", isAuth, shopController.postCart);
+router.post("/cart-delete-item", isAuth, shopController.postDeleteCartItem);
+router.get("/orders", isAuth, shopController.getOrders);
+router.post("/create-order", isAuth, shopController.postOrder);
 
 module.exports = router;
