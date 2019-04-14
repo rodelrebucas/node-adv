caedb3d07d138ac021854168412bfc1291a1b57f
rodel <rodelrebucas@gmail.com>
2019-04-14 14:04:38 +0800

    error handling in promise

--- node-advanced/promises.js
+++ node-advanced/promises.js
@@ -1,13 +1,18 @@
 // Promises replaces callback
 const delay = seconds =>
   new Promise((resolves, rejects) => {
+    if (seconds > 3) {
+      // Always throw errors in Promises inside reject
+      rejects(new Error(`${seconds} too long`));
+    }
     setTimeout(() => {
       // return value
       resolves(`Resolved after: ${seconds}`);
     }, seconds * 1000);
   });
-
-delay(2).then(returned => console.log(returned, ", Delayed for 2 seconds"));
+delay(4)
+  .then(returned => console.log(returned, ", Delayed for 2 seconds"))
+  .catch(err => console.log(`catching error: ${err}`));
 
 // First Tick
 // Returned after: 2 , Delayed for 2 seconds
