fde4e3a91ad72d86de75f93fd2f784f88d492816
rodel <rodelrebucas@gmail.com>
2021-02-25 22:50:13 +0800

    update comments and correct array index

--- customReadableStreams.js
+++ customReadableStreams.js
@@ -15,24 +15,34 @@ class StreamFromArray extends Readable {
 
   // Override
   _read() {
-    if (this.index <= this.array.length) {
-      //   const chunk = this.array[this.index];
+    if (this.index <= this.array.length - 1) {
+      // create the chunk to be read
       const chunk = {
         data: this.array[this.index],
-        index: this.index
+        index: this.index,
       };
+      // push the created chunk to be read
       this.push(chunk);
+
+      // increment index to next data
       this.index += 1;
     } else {
-      this.push(null); // signals no readable left
+
+      // signals no readable left
+      this.push(null);
     }
   }
 }
 
 const cardStream = new StreamFromArray(cards);
 
-// listen to data event
-cardStream.on("data", chunk => console.log(chunk));
+// Read and listen to data event
+cardStream.on("data", (chunk) => console.log(chunk));
 
 // listen to end event
 cardStream.on("end", () => console.log("Done reading."));
+
+// { data: 'Heart', index: 0 }
+// { data: 'Spade', index: 1 }
+// { data: 'Flower', index: 2 }
+// { data: 'Diamond', index: 3 }
