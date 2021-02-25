9e2484f8e4c4f26349d57b58b5aa89d0feaa131b
rodel <rodelrebucas@gmail.com>
2021-02-25 22:20:20 +0800

    correct comment on no. of promise delays

--- promises.js
+++ promises.js
@@ -1,5 +1,5 @@
 // Promises replaces callback
-const delay = seconds =>
+const delay = (seconds) =>
   new Promise((resolves, rejects) => {
     if (seconds > 3) {
       // Always throw errors in Promises inside reject
@@ -10,11 +10,12 @@ const delay = seconds =>
       resolves(`Resolved after: ${seconds}`);
     }, seconds * 1000);
   });
+
 delay(3)
-  .then(returned => console.log(returned, ", Delayed for 3 seconds"))
-  .catch(err => console.log(`catching error: ${err}`));
+  .then((returned) => console.log(returned, ", Delayed for 3 seconds"))
+  .catch((err) => console.log(`catching error: ${err}`));
 
 // First Tick
-// Returned after: 2 , Delayed for 2 seconds
+// Returned after: 2 , Delayed for 3 seconds
 
 console.log("First Tick");
